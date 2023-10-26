79e21a424f8c79c73ac9c38e6ceb25a2607f18e4
ermac_ <ermac_@mail.ru>
2023-10-26 07:25:19 +0300

    Добавлен код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Serialization;
+
+
+
+string[] array = {"123345", "qweert", "dsfggdgd", "gfhghfh"}; //исходный массив
+int numberOfSymbol = 2; //количество символов в новых массивах
+
+int sizeNewArray = 0;
+foreach (var item in array)
+{
+    sizeNewArray+=item.Length;
+}
+sizeNewArray  = math.
+Console.WriteLine(sizeNewArray);
+string[] newArray = new string[sizeNewArray];
+
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array[i].Length; j++)
+    {
+        newArray[k]=newArray[k] + array[i][j];
+    }
+}
+
+
