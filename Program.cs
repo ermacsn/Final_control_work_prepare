ef0ccdcd9c6b82aa6f0f6899bf0ee296bf3beb45
ermac_ <ermac_@mail.ru>
2023-10-27 07:33:26 +0300

    Обновлено решение

--- Program.cs
+++ Program.cs
@@ -1,26 +1,47 @@
-﻿using System.Xml.Serialization;
-
-
+﻿using System.Formats.Asn1;
+using System.Xml.Serialization;
+using Microsoft.Win32.SafeHandles;
 
 string[] array = {"123345", "qweert", "dsfggdgd", "gfhghfh"}; //исходный массив
-int numberOfSymbol = 2; //количество символов в новых массивах
+var random = new Random();
 
-int sizeNewArray = 0;
+
+string strArray = "";
 foreach (var item in array)
 {
-    sizeNewArray+=item.Length;
+    strArray += item;
 }
-sizeNewArray  = math.
-Console.WriteLine(sizeNewArray);
-string[] newArray = new string[sizeNewArray];
+Console.WriteLine(strArray);
 
-int k = 0;
-for (int i = 0; i < array.Length; i++)
+string[] resultArray = new string [strArray.Length]; // ограничивающее условие 
+
+int n = 0;
+for (int i = 0; i < resultArray.Length; i++)
 {
-    for (int j = 0; j < array[i].Length; j++)
+    if (n < strArray.Length - 1)
     {
-        newArray[k]=newArray[k] + array[i][j];
+        int k = random.Next(0,2);
+        if (strArray.Length - n < k) k = strArray.Length - n;
+        for (int j = 0; j < k; j++)
+        {
+            resultArray[i] = resultArray[i] + strArray[n];
+            n = n + 1;
+        }
     }
+    else resultArray[i] = "\n";    
+}
+   
+for (int a = 0; a < resultArray.Length; a++)
+{
+    if (resultArray[a] == "\n") break;
+    Console.WriteLine(resultArray[a]);    
 }
 
-
+int b =0;
+while (b < strArray.Length)
+{
+    b = b + random.Next(0,9);
+    if (b > strArray.Length) break; 
+    strArray = strArray.Insert(b, "\n");
+}
+Console.WriteLine(strArray); 
